d8fff5a724a10df9088a4c52038dec511c4e4922
sebastiencarreau-candid <sebastien.carreau@candidco.com>
2026-05-26 17:33:46 -0500

    Update gdrive.js

--- netlify/functions/gdrive.js
+++ netlify/functions/gdrive.js
@@ -36,7 +36,7 @@ exports.handler = async function(event) {
       `--${boundary}--`
     ].join('\r\n');
 
-    const uploadRes = await fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id,webViewLink', {
+    const uploadRes = await fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id,webViewLink&supportsAllDrives=true', {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${token}`,
@@ -52,7 +52,7 @@ exports.handler = async function(event) {
     }
 
     // Make file viewable by anyone with the link
-    await fetch(`https://www.googleapis.com/drive/v3/files/${uploadData.id}/permissions`, {
+    await fetch(`https://www.googleapis.com/drive/v3/files/${uploadData.id}/permissions?supportsAllDrives=true`, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${token}`,
