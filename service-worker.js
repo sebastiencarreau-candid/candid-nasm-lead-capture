86f5e9ee237f458c8678f328ecdd2d50c7dccf84
sebastiencarreau-candid <sebastien.carreau@candidco.com>
2026-05-27 15:33:20 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,10 @@
-const CACHE_NAME = 'candidpro-nasm-v2';
+const CACHE_NAME = 'candidpro-nasm-v3';
 const ASSETS = ['/', '/index.html'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
